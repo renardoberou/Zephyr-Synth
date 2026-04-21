d1557da41a26e3577df1b6add348a37b5eb20fd0
renardoberou <renardoberou@gmail.com>
2026-04-21 14:28:41 -0300

    Refactor MPE class to improve MIDI handling

--- src/mpe.js
+++ src/mpe.js
@@ -1,6 +1,6 @@
 export class MPE {
   /**
-   * @param {unknown} synth
+   * @param {Synth} synth
    */
   constructor(synth) {
     this.synth = synth;
@@ -10,6 +10,16 @@ export class MPE {
    * @param {MIDIMessageEvent} e
    */
   handleMIDI(e) {
-    console.log("MIDI:", e.data);
+    const [status, data1, data2] = e.data;
+    const type = status & 0xf0;
+
+    if (type === 0x90 && data2 > 0) {
+      const freq = 440 * Math.pow(2, (data1 - 69) / 12);
+      this.synth.noteOn(freq);
+    }
+
+    if (type === 0x80 || (type === 0x90 && data2 === 0)) {
+      this.synth.noteOff();
+    }
   }
 }
