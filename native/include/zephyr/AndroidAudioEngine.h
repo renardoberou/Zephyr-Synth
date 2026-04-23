3f56ba312c3ffa23d458ad970ea45f4419ba7ae0
renardoberou <renardoberou@gmail.com>
2026-04-23 02:01:52 -0300

    Add Android Oboe engine wrapper header

---
+++ native/include/zephyr/AndroidAudioEngine.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <cstdint>
+#include <memory>
+
+namespace oboe {
+class AudioStream;
+class AudioStreamBuilder;
+class MidiInputStream;
+class Result;
+} // namespace oboe
+
+namespace zephyr {
+
+class MidiTranslator;
+class ZephyrEngine;
+
+class AndroidAudioEngine {
+public:
+  AndroidAudioEngine();
+  ~AndroidAudioEngine();
+
+  bool start();
+  void stop();
+
+  bool handleMidiMessage(const std::uint8_t* data, std::size_t size);
+
+private:
+  class Impl;
+  std::unique_ptr<Impl> impl_;
+};
+
+} // namespace zephyr
